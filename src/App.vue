795c8b04bbc14121616e32971e37b2475db85c4f
LuciNyan <453440680@qq.com>
2017-05-02 15:31:15 +0800

    Router

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <h1>Haru!</h1>
+    <hr>
+    <router-link to="/">Home</router-link>
+    <router-link to="/users/10">Users for 10</router-link>
+    <router-link to="/users/12">Users for 12</router-link>
+    <hr>
     <router-view></router-view>
   </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import Users from '@/Users'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
+    { path: '/', name: 'Hello', component: Hello },
+    { path: '/users/:teamId', name: '1', component: Users }
   ]
 })
